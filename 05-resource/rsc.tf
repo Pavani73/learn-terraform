f762e4fc19e27de1e865754820c5c289d3cb243c
Pavani <pavani.chitla@gmail.com>
2023-06-09 17:25:14 +1000

    data source prctice

--- 05-resource/rsc.tf
+++ 05-resource/rsc.tf
@@ -1,11 +1,11 @@
-resource "aws_instance" "web" {
+/*resource "aws_instance" "web" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
 
   tags = {
     Name = "HelloWorld"
   }
-}
+}*/
 
 data "aws_ami" "example" {
   owners = ["973714476881"]
