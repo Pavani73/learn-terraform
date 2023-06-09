f729f16513819efa919fb608878cb7815b074236
Pavani <pavani.chitla@gmail.com>
2023-06-09 15:19:02 +1000

    data source prctice

--- 04-data-source/data.tf
+++ 04-data-source/data.tf
@@ -27,13 +27,11 @@ output "sgid"{
 }
 
 data "aws_ami" "example" {
-
+  owners           = ["973714476881"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice "
-  owners           = ["973714476881"]
-
-}
-output "ami"{
+ }
+output "ami" {
   value = data.aws_ami.example.id
 }
 provider "aws"{
