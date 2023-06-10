a26a7a76c0bd28d53727e16e1a80007cb84b9e88
Pavani <pavani.chitla@gmail.com>
2023-06-10 23:26:02 +1000

    data source prctice

--- 11-functions/main.tf
+++ 11-functions/main.tf
@@ -6,5 +6,31 @@ variable "class"{
  output "class"{
    value = upper(var.class)
  }
+variable "fruits_count"{
+  default = ["apple","banana","orange"]
+}
 
+output "fruits_count" {
+      value = length(var.fruits)
+}
+variable "classes"{
+
+  default = {
+    devops = {
+      name = "devops"
+      topics = ["jenkins", "dockers"]
+    }
+   aws = {
+     name = "aws"
+   }
+  }
+}
+output "devops_topics" {
+    value = var.classes["devops"]["topics"]
+}
+
+output "aws_topics"{
+
+  value = lookup(var.classes, "aws", null)
+}
 
